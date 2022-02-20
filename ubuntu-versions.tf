ee1e7e09c6d9c2aff09022db3aba43bb1ae84999
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-21 00:18:00 +0100

    Initial commit

--- ubuntu-versions.tf
+++ ubuntu-versions.tf
@@ -7,10 +7,23 @@ variable "ubuntu_1804_sku" {
   description = "SKU for Ubuntu 18.04 LTS"
   default     = "ubuntu-os-cloud/ubuntu-1804-lts"
 }
-
 variable "ubuntu_2004_sku" {
   type        = string
   description = "SKU for Ubuntu 20.04 LTS"
   default     = "ubuntu-os-cloud/ubuntu-2004-lts"
 }
-
+variable "ubuntu_pro_1604_sku" {
+  type        = string
+  description = "SKU for Ubuntu PRO 16.04 LTS"
+  default     = "ubuntu-os-pro-cloud/ubuntu-pro-1604-lts"
+}
+variable "ubuntu_pro_1804_sku" {
+  type        = string
+  description = "SKU for Ubuntu PRO 18.04 LTS"
+  default     = "ubuntu-os-pro-cloud/ubuntu-pro-1804-lts"
+}
+variable "ubuntu_pro_2004_sku" {
+  type        = string
+  description = "SKU for Ubuntu PRO 20.04 LTS"
+  default     = "ubuntu-os-pro-cloud/ubuntu-pro-2004-lts"
+}
